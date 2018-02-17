29aff610e70a44f8b45e0dfbc840e98609b710be
bikram-snapwizer <bikram.choudhury@snapwiz.com>
2018-02-18 03:46:24 +0530

    adding firebase.config for prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD4PZzHo0XRXKbf-FxMe7yNpvMrl8e1604",
+    authDomain: "wishlist-6d1f0.firebaseapp.com",
+    databaseURL: "https://wishlist-6d1f0.firebaseio.com",
+    projectId: "wishlist-6d1f0",
+    storageBucket: "",
+    messagingSenderId: "168177742722"
+  }
 };
